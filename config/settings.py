1f2948c1b9ca0b4f50dfd7b1ae252c8c3674ae82
Ryan Kim <iawal86@gmail.com>
2020-10-30 17:48:39 -0700

    created email verification with using mailgun

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,12 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 MEDIA_URL = "/media/"
+
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "postmaster@sandbox8183791d969545b8b0b9e60182c8f0cb.mailgun.org"
